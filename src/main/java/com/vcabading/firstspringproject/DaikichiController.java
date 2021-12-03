f06e74f258f1c5fdec248765c9b22844ad37c67c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-02 22:21:08 -0800

    implement /daikishi/today route

--- src/main/java/com/vcabading/firstspringproject/DaikichiController.java
+++ src/main/java/com/vcabading/firstspringproject/DaikichiController.java
@@ -12,5 +12,8 @@ public class DaikichiController {
 		return "Welcome";
 	}
 
-	
+	@RequestMapping("/today")
+	public String daikichiToday() {
+		return "Today, you will find luck in all your endeavors!";
+	}
 }
