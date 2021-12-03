6ea1d8a88d14ce8c7193c9903423353ab8e27dbb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-03 00:54:15 -0800

    implement lotto numbers route

--- src/main/java/com/vcabading/firstspringproject/DaikichiController.java
+++ src/main/java/com/vcabading/firstspringproject/DaikichiController.java
@@ -27,4 +27,29 @@ public class DaikichiController {
 	public String daikichiTravelCity(@PathVariable("city") String city) {
 		return "Congratulations!  You will soon travel to " + city + "!";
 	}
+	
+	@RequestMapping("lotto/{inputNumber}")
+	public String daikichiLottoNumber(@PathVariable("inputNumber") String inputNumber) {
+		if (DaikichiController.isNumeric(inputNumber)) {
+			int number = Integer.parseInt(inputNumber);
+			if (number % 2 == 0) {
+				return "You will take a grand journey in the near future, but be weary of etempting offers.";
+			} else {
+				return "You have enjoyed the fruits of your labor but now is a great timeto spend time with family and friends.";
+			}
+		}
+		return "Invalid number";
+	}
+	
+	public static boolean isNumeric(String strNum) {
+	    if (strNum == null) {
+	        return false;
+	    }
+	    try {
+	        Integer d = Integer.parseInt(strNum);
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
+	    return true;
+	}
 }
